65fb60e1ce64a9b89c37e240080160ed361fbaa3
KrishhnaT <tupekrishhna@gmail.com>
2025-01-16 19:27:57 +0530

    array basics

---
+++ Arrays/ArrayInput.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ int main(){
+     int arr[5];
+     for (int i=0;i<=4;i++){
+          cout<<"Enter the "<<i<< " element:";
+          cin>>arr[i];
+     }
+     for(int j=0;j<=4;j++){
+            cout<<arr[j]<<" ";
+     }
+     return 0;
+
+ }
